5b03a81c7c17d69edbbc1b1932d67c8c418a43c8
Trung Tin <haphamtrungtin@gmail.com>
2023-02-07 15:36:48 +0700

    .

---
+++ SqlQuery_1.sql
@@ -0,0 +1,14 @@
+﻿SELECT 
+prd.Id,
+prd.ProductMainImg,
+prd.SKU,
+prd.ProductName,
+prd.CreatedAt,
+prc.UpdatedAt,
+prc.AvaiableStock,
+prd.Price,
+prc.ProductChannelStatus
+FROM ProductChannel prc
+JOIN Product prd ON prc.ProductId = prd.Id
+JOIN Channel chn ON chn.Id = prc.ChannelId
+WHERE chn.Id = 3 AND ProductChannelStatus = 'ACTIVE'
